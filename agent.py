94f0cecbb539d3810b3c1c23f6a086548cda104a
Nathan Bailey <nathanbaileyw@gmail.com>
2023-07-30 11:21:23 +0100

    Added files to test popping the stack

--- agent.py
+++ agent.py
@@ -28,8 +28,8 @@ class Agent():
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.episode_durations = []
         self.cache_recall = CacheRecall.CacheRecall(memory_size=MEMORY_SIZE)
-        self.policy_net = model.FlappyNet(input_dim=self.input_dim, output_dim=self.output_dim).to(self.device)
-        self.target_net = model.FlappyNet(input_dim=self.input_dim, output_dim=self.output_dim).to(self.device)
+        self.policy_net = model.FlappyNetv2(input_dim=self.input_dim, output_dim=self.output_dim).to(self.device)
+        self.target_net = model.FlappyNetv2(input_dim=self.input_dim, output_dim=self.output_dim).to(self.device)
         for param in self.target_net.parameters():
             param.requires_grad = False
         self.target_net.load_state_dict(self.policy_net.state_dict())
@@ -65,7 +65,7 @@ class Agent():
             means = torch.cat((torch.zeros(99), means))
             plt.plot(means.numpy())
         plt.pause(0.001)  # pause a bit so that plots are updated
-        plt.savefig('training.png')
+        plt.savefig('trainingv2.png')
 
     @torch.no_grad()
     def take_action(self, state):
@@ -126,6 +126,8 @@ class Agent():
                 done = env.game_over()
                 if done:
                     next_state = None
+                if len(self.cache_recall) >= self.MEMORY_SIZE:
+                    self.cache_recall.memory.popleft()
                 self.cache_recall.cache((state, next_state, action, reward, done))
                 state = next_state
                 self.optimize_model()
@@ -139,7 +141,7 @@ class Agent():
                     print("EPS: {}".format(self.eps))
                     print("Durations: {}".format(c+1))
                     print("Score: {}".format(env.score()))
-                    torch.save(self.target_net.state_dict(), 'target_net.pt')
-                    torch.save(self.policy_net.state_dict(), 'policy_net.pt')
+                    torch.save(self.target_net.state_dict(), 'target_netv2.pt')
+                    torch.save(self.policy_net.state_dict(), 'policy_netv2.pt')
                     #Start a new episode
                     break
